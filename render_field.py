a2ed7c663a724e109177c86e58a1b4d7c8b4ea85
JoseVictorMarques <josevictor.marques1998@gmail.com>
2020-03-29 13:22:52 -0300

    Atualizando alguns parametros

--- render_field.py
+++ render_field.py
@@ -1,9 +1,10 @@
 import sys
 import pygame
-import const_values
+import screeen
+import constants
 
 
-def function render_field (background, screen, game, currentPlayerId)
+def function render_field (screen, game)
     """
     this function renders some aspects of the field
     """
@@ -11,7 +12,7 @@ def function render_field (background, screen, game, currentPlayerId)
     width, height = const_values.WIDTH, const_values.HEIGHT
 
     # fill the context with the background color
-     screen.fill (game.background_color)
+    screen.fill (game.background_color)
 
     # draw the center of circle
     pygame.draw.circle(screen, const_values.WHITE, (int(width / 2), int(height / 2)), 70, 5)
