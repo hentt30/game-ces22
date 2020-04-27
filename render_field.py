f4a8ced92320ec0c231b5b2de299c6df967a0105
JoseVictorMarques <josevictor.marques1998@gmail.com>
2020-04-26 23:39:32 -0300

    Ajeitando alguns detalhes

--- render_field.py
+++ render_field.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-import screeen
+import screen
 import constants
 
 
@@ -9,27 +9,27 @@ def render_field (screen, game):
     this function renders some aspects of the field
     """
     # simplify variable names
-    width, height = const_values.WIDTH, const_values.HEIGHT
+    width, height = constants.WIDTH, constants.HEIGHT
 
     # fill the context with the background color
     screen.fill (game.background_color)
 
     # draw the center of circle
-    pygame.draw.circle(screen, const_values.WHITE, (int(width / 2), int(height / 2)), 70, 5)
+    pygame.draw.circle(screen, constants.WHITE, (int(width / 2), int(height / 2)), 70, 5)
 
     # draw the rectangle of field
-    pygame.draw.rect(screen, const_values.WHITE, (0, 0, width, height), 5)
+    pygame.draw.rect(screen, constants.WHITE, (0, 0, width, height), 5)
 
     # draw the defense areas
-    pygame.draw.rect(screen, const_values.WHITE, (0, int(height / 2) - 150, 150, 300), 5)
-    pygame.draw.rect(screen, const_values.WHITE, (width - 150, int(height / 2)- 150, 150, 300), 5)
+    pygame.draw.rect(screen, constants.WHITE, (0, int(height / 2) - 150, 150, 300), 5)
+    pygame.draw.rect(screen, constants.WHITE, (width - 150, int(height / 2)- 150, 150, 300), 5)
 
     # draw the goals
-    pygame.draw.rect(screen, const_values.BLACK, (0, const.GOAL_Y1, 5, const_values.GOAL_WIDTH))
-    pygame.draw.rect(screen, const_values.BLACK, (width - 5, const.GOAL_Y1, 5, const_values.GOAL_WIDTH))
+    pygame.draw.rect(screen, constants.BLACK, (0, constants.GOAL_Y1, 5, constants.GOAL_WIDTH))
+    pygame.draw.rect(screen, constants.BLACK, (width - 5, constants.GOAL_Y1, 5, constants.GOAL_WIDTH))
 
     # draw the field divider
-    pygame.draw.rect(screen, const_values.WHITE, (width / 2, 0, 3, height))
+    pygame.draw.rect(screen, constants.WHITE, (width / 2, 0, 3, height))
 
     # pause the game
     screen.blit(pause_image, (width / 2 - 32, height - 70))
